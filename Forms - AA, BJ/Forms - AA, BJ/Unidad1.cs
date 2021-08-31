c4709907b489ec094dcc9b644c94c6a1d89387fe
Ale Aiassa <aleaiassa2008@hotmail.com>
2021-08-30 21:39:01 -0300

    Cambios + Argumentos (Ver de cambiar a variables string directamente)

--- Forms - AA, BJ/Forms - AA, BJ/Unidad1.cs
+++ Forms - AA, BJ/Forms - AA, BJ/Unidad1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,15 +10,26 @@ namespace Forms___AA__BJ
 {
     class Unidad1 //System.Globalization
     {
-        public Function Expresion { get; set; }
+        public Function Funcion { get; set; }
+        public Expression ExpresionXi { get; set; }
+        public Expression ExpresionXd { get; set; }
+        public Expression ExpresionXr { get; set; }
+        public Argument ArgumentoXi { get; set; }
+        public Argument ArgumentoXd { get; set; }
+        public Argument ArgumentoXr { get; set; }
 
-        private double ResultadoFuncion(double x)
-        {
-            return Expresion.calculate(x);
-        }
+        //private double ResultadoFuncion(double x)
+        //{
+        //    return Expresion.calculate();
+        //}
         public Salida Calcular(string Metodo, string Fx, double Xi, double Xd, int Iteraciones, double Toleracia)
         {
-            Expresion = new Function(Fx);
+            Funcion = new Function( "Funcion(x) = " + Fx);
+            ArgumentoXi = new Argument("ArgumentoXi = " + Xi.ToString(CultureInfo.CreateSpecificCulture("en-GB")));
+            ArgumentoXd = new Argument("ArgumentoXd = " + Xd.ToString(CultureInfo.CreateSpecificCulture("en-GB")));
+            ExpresionXi= new Expression("Funcion(x)", Funcion, ArgumentoXi);
+            ExpresionXd = new Expression("Funcion(x)", Funcion, ArgumentoXd);
+
             switch (Metodo)
             {
                 case "Bisección":
@@ -25,22 +37,22 @@ namespace Forms___AA__BJ
                     double ErrorRelativo = 1;
                     Salida Resultado;
                     double Xr = 0;
+                    if (ExpresionXi.calculate() * ExpresionXd.calculate() == 0)
+                    {
+                        if (ExpresionXi.calculate() == 0)
+                        {
+                            Resultado = new Salida("Si, Xi ya era Raiz", Xi.ToString(), IteracionActual, ErrorRelativo);
+                            return Resultado;
+                        }
+                        else
+                        {
+                            Resultado = new Salida("Si, Xd ya era Raiz", Xd.ToString(), IteracionActual, ErrorRelativo);
+                            return Resultado;
+                        }
+                    }
                     while ((IteracionActual < Iteraciones))
                     {
-                        if (ResultadoFuncion(Xi) * ResultadoFuncion(Xd) == 0)
-                        {
-                            if (ResultadoFuncion(Xi)==0)
-                            {
-                                Resultado = new Salida("Si, Xi ya era Raiz", Xi.ToString(), IteracionActual, ErrorRelativo);
-                                return Resultado;
-                            }
-                            else
-                            {
-                                Resultado = new Salida("Si, Xd ya era Raiz", Xd.ToString(), IteracionActual, ErrorRelativo);
-                                return Resultado;
-                            }
-                        }
-                        Xr = (Xd * Xi) / 2;
+                        Xr = (Xd + Xi) / 2;
                         IteracionActual += 1; // Preguntar donde va esto
                         //ErrorRelativo = 0; //Ver esto
                         if (ErrorRelativo < Toleracia)
@@ -48,14 +60,17 @@ namespace Forms___AA__BJ
                             Resultado = new Salida("Si, error relativo aceptable", Xr.ToString(), IteracionActual, ErrorRelativo);
                             return Resultado;
                         }
-                        if (ResultadoFuncion(Xr) == 0)
+                        ArgumentoXr = new Argument("ArgumentoXr = " + Xr.ToString(CultureInfo.CreateSpecificCulture("en-GB")));
+                        ExpresionXr = new Expression("Funcion(x)", Funcion, ArgumentoXr);
+                        var asd = ExpresionXr.calculate();
+                        if (ExpresionXr.calculate() == 0)
                         {
                             Resultado = new Salida("Si", Xr.ToString(), IteracionActual, ErrorRelativo);
                             return Resultado;
                         }
                         else
                         {
-                            if (ResultadoFuncion(Xi) * ResultadoFuncion(Xd) > 0)
+                            if (ExpresionXi.calculate() * ExpresionXd.calculate() > 0)
                             {
                                 Xi = Xr;
                             }
