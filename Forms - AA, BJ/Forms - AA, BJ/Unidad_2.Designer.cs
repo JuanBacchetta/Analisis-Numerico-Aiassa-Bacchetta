a6b45e5700d5e543da2b714fef3bb3f2cbde8486
Ale Aiassa <aleaiassa2008@hotmail.com>
2021-09-20 21:24:01 -0300

    Parte Visual - Unidad 2

--- Forms - AA, BJ/Forms - AA, BJ/Unidad_2.Designer.cs
+++ Forms - AA, BJ/Forms - AA, BJ/Unidad_2.Designer.cs
@@ -29,12 +29,91 @@ namespace Forms___AA__BJ
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(447, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "GENERAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(447, 59);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "CALCULAR";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(398, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(25, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(64, 32);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(324, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "DIMENSIÓN";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(64, 115);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(359, 214);
+            this.panel1.TabIndex = 5;
+            // 
+            // Unidad_2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(553, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Unidad_2";
             this.Text = "Unidad_2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
     }
 }
--- Forms - AA, BJ/Forms - AA, BJ/Unidad_2.cs
+++ Forms - AA, BJ/Forms - AA, BJ/Unidad_2.cs
@@ -16,5 +16,54 @@ namespace Forms___AA__BJ
         {
             InitializeComponent();
         }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int pointX = 30;
+                int pointY = 30;
+                panel1.Controls.Clear();
+                textBox1.Text = ((int)double.Parse(textBox1.Text.Replace(".", ","))).ToString();
+                if (int.Parse(textBox1.Text) > 5)
+                {
+                    textBox1.Text = 5.ToString();
+                }
+                if (int.Parse(textBox1.Text) < 0)
+                {
+                    textBox1.Text = 0.ToString();
+                }
+                for (int j = 0; j < int.Parse(textBox1.Text) + 1; j++)
+                {
+                    for (int i = 0; i < int.Parse(textBox1.Text); i++)
+                    {
+                        TextBox txt = new TextBox();
+                        txt.Location = new Point(pointX, pointY);
+                        txt.Name = $"txt{i}{j}";
+                        txt.AutoSize = false;
+                        txt.Size = new Size(30, 30);
+                        txt.Font = new Font("Microsoft Sans Serif", 12);
+                        panel1.Controls.Add(txt);
+                        pointY += 50;
+                    }
+                    pointX += 50;
+                    pointY = 30;
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
     }
 }
