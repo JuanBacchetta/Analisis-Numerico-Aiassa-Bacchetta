52964a249bb3ed8f1afa8bc6dd02af2287b041e5
Ale-Aiassa <aleaiassa2008@hotmail.com>
2021-11-06 20:50:19 -0300

    U4

--- Forms - AA, BJ/Forms - AA, BJ/Unidad_4.cs
+++ Forms - AA, BJ/Forms - AA, BJ/Unidad_4.cs
@@ -37,6 +37,7 @@ namespace Forms___AA__BJ
             Function Funcion = new Function("Funcion(x) =" + textBox1.Text);
             Expression ExpresionXi = new Expression("Funcion(" + textBox2.Text.Replace(",", ".") + ")", Funcion);
             Expression ExpresionXf = new Expression("Funcion(" + textBox3.Text.Replace(",", ".") + ")", Funcion);
+            double h = (double.Parse(textBox3.Text.Replace(".", ",")) - double.Parse(textBox2.Text.Replace(".", ","))) / double.Parse(textBox4.Text);
             if (comboBox1.Text == "Trapecio Simple")
             {
                 textBox5.Text = (((ExpresionXf.calculate() + ExpresionXi.calculate()) * (double.Parse(textBox3.Text.Replace(".", ",")) - double.Parse(textBox2.Text.Replace(".", ","))) / 2).ToString());
@@ -44,7 +45,6 @@ namespace Forms___AA__BJ
             if (comboBox1.Text == "Trapecio Multiple")
             {
                 textBox5.Text = "0";
-                double h = (double.Parse(textBox3.Text.Replace(".",",")) - double.Parse(textBox2.Text.Replace(".",",")))/double.Parse(textBox4.Text);
                 for (int i = 1; i < int.Parse(textBox4.Text); i++)
                 {
                     Expression expresionXParcial = new Expression("Funcion(" + (double.Parse(textBox2.Text.Replace(".", ",")) + i * h).ToString().Replace(",",".") + ")", Funcion);
@@ -56,15 +56,60 @@ namespace Forms___AA__BJ
             }
             if (comboBox1.Text == "Simpson 1/3 Simple")
             {
-
+                h = (double.Parse(textBox3.Text.Replace(".", ",")) - double.Parse(textBox2.Text.Replace(".", ",")))/2;
+                Expression expresionXPromedio = new Expression("Funcion(" + ((double.Parse(textBox2.Text.Replace(".", ",")) + double.Parse(textBox3.Text.Replace(".", ",")))/2).ToString().Replace(",", ".") + ")", Funcion);
+                textBox5.Text = ((h / 3) * (ExpresionXi.calculate() + 4 * expresionXPromedio.calculate() + ExpresionXf.calculate())).ToString().Replace(".", ",");
             }
             if (comboBox1.Text == "Simpson 1/3 Multiple")
             {
-
+                if (Math.Floor(double.Parse(textBox4.Text)/2) == double.Parse(textBox4.Text)/2)
+                {
+                    textBox5.Text = "0";
+                    textBox5.Text = (ExpresionXi.calculate() + ExpresionXf.calculate()).ToString().Replace(".", ",");
+                    for (int i = 1; i < double.Parse(textBox4.Text); i += 2)
+                    {
+                        Expression expresionXParcial = new Expression("Funcion(" + (double.Parse(textBox2.Text.Replace(".", ",")) + i * h).ToString().Replace(",", ".") + ")", Funcion);
+                        textBox5.Text = (double.Parse(textBox5.Text) + expresionXParcial.calculate() * 4).ToString().Replace(".", ",");
+                    }
+                    for (int i = 2; i < double.Parse(textBox4.Text); i += 2)
+                    {
+                        Expression expresionXParcial = new Expression("Funcion(" + (double.Parse(textBox2.Text.Replace(".", ",")) + i * h).ToString().Replace(",", ".") + ")", Funcion);
+                        textBox5.Text = (double.Parse(textBox5.Text) + expresionXParcial.calculate() * 2).ToString().Replace(".", ",");
+                    }
+                    textBox5.Text = (double.Parse(textBox5.Text) * (h / 3)).ToString().Replace(".", ",");
+                }
             }
             if (comboBox1.Text == "Simpson 3/8")
             {
-
+                if (Math.Floor(double.Parse(textBox4.Text) / 2) != double.Parse(textBox4.Text) / 2 && double.Parse(textBox4.Text) >= 3)
+                {
+                    textBox5.Text = "0";
+                    if (double.Parse(textBox4.Text) > 3)
+                    {
+                        ExpresionXf = new Expression("Funcion(" + (double.Parse(textBox3.Text.Replace(".", ",")) - 3 * h).ToString().Replace(",", ".") + ")", Funcion);
+                        textBox5.Text = (ExpresionXi.calculate() + ExpresionXf.calculate()).ToString().Replace(".", ",");
+                        for (int i = 1; i < double.Parse(textBox4.Text) - 3; i += 2)
+                        {
+                            Expression expresionXParcial = new Expression("Funcion(" + (double.Parse(textBox2.Text.Replace(".", ",")) + i * h).ToString().Replace(",", ".") + ")", Funcion);
+                            textBox5.Text = (double.Parse(textBox5.Text) + expresionXParcial.calculate() * 4).ToString().Replace(".", ",");
+                        }
+                        for (int i = 2; i < double.Parse(textBox4.Text) - 3; i += 2)
+                        {
+                            Expression expresionXParcial = new Expression("Funcion(" + (double.Parse(textBox2.Text.Replace(".", ",")) + i * h).ToString().Replace(",", ".") + ")", Funcion);
+                            textBox5.Text = (double.Parse(textBox5.Text) + expresionXParcial.calculate() * 2).ToString().Replace(".", ",");
+                        }
+                        textBox5.Text = (double.Parse(textBox5.Text) * (h / 3)).ToString().Replace(".", ",");
+                        ExpresionXi = ExpresionXf;
+                    }
+                    Expression expresionXIntermedia1 = new Expression("Funcion(" + (double.Parse(textBox3.Text.Replace(".", ",")) - 2 * h).ToString().Replace(",", ".") + ")", Funcion);
+                    Expression expresionXIntermedia2 = new Expression("Funcion(" + (double.Parse(textBox3.Text.Replace(".", ",")) - 1 * h).ToString().Replace(",", ".") + ")", Funcion);
+                    ExpresionXf = new Expression("Funcion(" + textBox3.Text.Replace(",", ".") + ")", Funcion);
+                    var sar = ExpresionXi.calculate();
+                    var asa = expresionXIntermedia1.calculate() * 3;
+                    var sa = expresionXIntermedia2.calculate() * 3;
+                    var rasa = ExpresionXf.calculate();
+                    textBox5.Text = (double.Parse(textBox5.Text) + (0.375) * h * (ExpresionXi.calculate() + expresionXIntermedia1.calculate() * 3 + expresionXIntermedia2.calculate() * 3 + ExpresionXf.calculate())).ToString().Replace(".", ",");
+                }
             }
         }
     }
